1d04a964dccc92eaad7a0acfef65f9e8ac84ac3e
Oleg <yastreb92@inbox.ru>
2015-11-29 23:51:27 +0300

    2

---
+++ js/common.js
@@ -0,0 +1,18 @@
+$(window).load(function() {
+//$(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+	  
+	items:1,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:2000,
+    //autoplayHoverPause:true,
+    autoplaySpeed:4000  
+	
+	
+ 
+  });
+ 
+});

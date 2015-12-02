14d23371a6e5e0347358ddb9efdc98a6b6e7a0c8
Oleg <yastreb92@inbox.ru>
2015-12-03 00:59:15 +0300

    second carousel

--- js/common.js
+++ js/common.js
@@ -3,16 +3,29 @@ $(window).load(function() {
  
   $("#owl-demo").owlCarousel({
 	  
-	items:1,
-    loop:true,
-    margin:10,
-    autoplay:true,
-    autoplayTimeout:2000,
+	items: 1,
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 2000,
     //autoplayHoverPause:true,
-    autoplaySpeed:4000  
+    autoplaySpeed: 4000  
 	
 	
  
   });
  
+	$("#owl-demo-1").owlCarousel({
+	  
+	items: 4,
+	loop: true,
+	margin: 35,
+	//autoplay: true,
+	autoplayTimeout: 2000,
+	//autoplayHoverPause:true,
+	autoplaySpeed: 4000
+	
+	
+ 
+  });
 });

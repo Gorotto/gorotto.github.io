99f81e69b209ba86ccb4f21f99721ea834a1093e
Oleg <yastreb92@inbox.ru>
2015-12-07 18:05:54 +0300

    js

--- js/common.js
+++ js/common.js
@@ -1,33 +1,43 @@
-$(window).load(function() {
+$(window).load(function () {
 //$(document).ready(function() {
  
-  $("#owl-demo").owlCarousel({
-	  
-	items: 1,
-    loop: true,
-    margin: 10,
-    autoplay: true,
-    autoplayTimeout: 2000,
-    //autoplayHoverPause:true,
-    autoplaySpeed: 4000  
-	
-	
- 
-  });
+	$("#owl-demo").owlCarousel({
+		items : 1,
+		loop: true,
+		margin : 10,
+		autoplay: true,
+		autoplayTimeout: 2000,
+		//autoplayHoverPause:true,
+		autoplaySpeed: 4000
+	});
  
 	$("#owl-demo-1").owlCarousel({
-	  
-	items: 4,
-	loop: true,
-	margin: 35,
-	//autoplay: true,
-	//autoplayTimeout: 2000,
-	//autoplayHoverPause:true,
-	//autoplaySpeed: 4000,
-	nav : true,
-	navText: ["prev","next"],
-	pagination : true,
-		
+	
+		loop: true,
+		margin: 35,
+		nav : true,
+		navText: ["prev", "next"],
+		//pagination : true,
+		items: 4,
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1,
+				nav: true,
+				margine: 35
+			},
+			600: {
+				items: 2,
+				nav: false
+			},
+			1000: {
+				items: 4,
+				nav: true,
+				loop: false
+			}
+		}
+
+	
 	});
 	
 });
